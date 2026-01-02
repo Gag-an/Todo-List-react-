d585d353aca8528e0ab4d4400097787fa3ca5efa
Gagandeep Kaur <gagkaur274@gmail.com>
2026-01-02 12:05:43 +0530

    delete and uppercase

--- src/todo.jsx
+++ src/todo.jsx
@@ -1,10 +1,13 @@
+import { v4 as uuidv4 } from 'uuid';
 import{useState} from "react";
 export default function Todo(){
-    let[todos,setTodos]=useState(["sample"]); /*todos trigger add button*/
+    let[todos,setTodos]=useState([{task:"sample", id:uuidv4()}]); /*todos trigger add button*/
     let[newTodo,setNewTodo]=useState("");  /*newTodo is the value of the input field*/
 
     let addNewTask=()=>{
-        setTodos([...todos,newTodo]);
+        setTodos((prevTodos)=>{
+            return[...prevTodos,{task:newTodo, id:uuidv4()}];
+        })
         setNewTodo(""); /*reset value of placeholder to empty after adding*/
     };
 
@@ -12,6 +15,25 @@ export default function Todo(){
         setNewTodo(event.target.value);
         
     }
+
+    let deleteTodo=(id)=>{
+        setTodos((prevTodos)=>{
+            return prevTodos.filter((todo)=> todo.id !=id);
+        });
+    }
+
+
+    let upperCaseAll=()=>{
+        setTodos( (prevTodos)=>
+            todos.map((todo)=>{
+                return{
+                    ...todo,
+                    task: todo.task.toUpperCase(),
+                };
+            })
+        );
+        
+    };
     return(
         <div>
             <h1>ToDO List</h1>
@@ -25,10 +47,16 @@ export default function Todo(){
             <ul>
                 {
                     todos.map((todo) =>(
-                        <li>{todo}</li>
+                        <li key={todo.id}>
+                            <span>{todo.task}</span>
+                            &nbsp;&nbsp;&nbsp;&nbsp;
+                            <button onClick={()=>deleteTodo(todo.id)}>Delete</button>
+                        </li>
                     ))
                 }
             </ul>
+
+            <button onClick={upperCaseAll}>uppercase All</button>
         </div>
     );
 }
